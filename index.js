eebf37b41a984691401df0df6f91150268dee4b7
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-11-08 11:43:53 -0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5174'],
+    credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser())
 
 
 console.log(process.env.DB_PASS)
@@ -23,10 +28,30 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares
+const logger = async (req, res, next) => {
+    console.log('called:', req.host, req.originalUrl)
+    next();
+}
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const roomsCollection = client.db('hotelBooking').collection('Rooms');
         const bookCollection = client.db('bookedRoom').collection('Booked');
 
@@ -37,7 +62,12 @@ async function run() {
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1h'
             });
-            res.send(token)
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+            .send({ success: true })
         })
         // get room data
         app.get('/rooms', async (req, res) => {
@@ -55,7 +85,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post("/booked", async (req, res) => {
+        app.post("/booked", logger, async (req, res) => {
             const newBooked = req.body;
             console.log(newBooked)
             // database e send
@@ -64,11 +94,12 @@ async function run() {
         })
 
         // get booking data
-        app.get('/booked', async (req, res) => {
+        app.get('/booked', logger, async (req, res) => {
             let query = {}
             if (req.query?.email) {
                 query = { email: req.query.email }
             }
+            console.log('tok tok', req.cookies.token)
             const cursor = bookCollection.find();
             const result = await cursor.toArray();
             res.send(result)
