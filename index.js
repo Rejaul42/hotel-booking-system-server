8d55618827e0115e3a8f78da1462b83d768d1be7
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-11-08 10:29:34 -0600

    create access tocken

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ async function run() {
         app.post('/jwt', logger, async (req, res) => {
             const user = req.body;
             console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send(token)
         })
         // get room data
         app.get('/rooms', async (req, res) => {
